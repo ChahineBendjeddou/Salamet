cde46ece57258156195ad258b798a68d06c19ac6
ChahineBendjeddou <chahine.bendjeddou99@gmail.com>
2022-03-30 02:10:10 +0100

    Adding an Express Server

---
+++ server.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3000
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+    })
+}
+
+app.listen(port, (err) => {
+    if (err) return console.error(err)
+    console.log(`Server is up on port ${port}!`)
+})
