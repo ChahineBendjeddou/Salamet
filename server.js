2af43799e1bb4867bc5da76127d54023c7a749cc
ChahineBendjeddou <chahine.bendjeddou99@gmail.com>
2022-03-30 02:14:30 +0100

    Testing

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = express()
 const port = process.env.PORT || 3000
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('build'))
-    app.get('*', (req, res) => {
-        req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-    })
 }
 
+app.use(express.static('build'))
+app.get('*', (req, res) => {
+    req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+})
 app.listen(port, (err) => {
     if (err) return console.error(err)
     console.log(`Server is up on port ${port}!`)
