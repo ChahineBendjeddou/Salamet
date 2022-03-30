8f31fd626ca36e1da6aa809ea2084aa42efe5686
ChahineBendjeddou <chahine.bendjeddou99@gmail.com>
2022-03-30 02:13:10 +0100

    Correcting the get route for '*'

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('build'))
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+        req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
     })
 }
 
