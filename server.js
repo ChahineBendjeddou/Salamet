34f1feaec644cb4cab1b7423655297091fa90800
ChahineBendjeddou <chahine.bendjeddou99@gmail.com>
2022-04-01 20:27:23 +0100

    setting Cors middleware & the prod ENV

--- server.js
+++ server.js
@@ -1,5 +1,34 @@
 const express = require('express')
+const path = require('path')
 const app = express()
+
+
+
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://salamet.herokuapp.com/']
+const corsOptions = {
+    origin: function (origin, callback) {
+        console.log("** Origin of request " + origin)
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            console.log("Origin acceptable")
+            callback(null, true)
+        } else {
+            console.log("Origin rejected")
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+app.use(cors(corsOptions))
+
 
 app.get('/api', (req, res) => {
 
