5fd75ae3451330667cb3d1ca0e5ca08a637b6bb8
ChahineBendjeddou <chahine.bendjeddou99@gmail.com>
2022-04-01 19:32:11 +0100

    Removing the response from the api route

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/api', (req, res) => {
-    res.json({ "users": ["userOne", "userTwo", "userThree"] })
+
 })
 
 app.listen(5000, () => console.log('Server Started on port 5000'))
