35059062d06068d78e56840024207aa57d5ce282
ChahineBendjeddou <chahine.bendjeddou99@gmail.com>
2022-04-02 00:05:24 +0100

    undo testing

--- server.js
+++ server.js
@@ -4,6 +4,26 @@ const app = express()
 const cors = require('cors')
 
 
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://salamet.herokuapp.com/']
+const corsOptions = {
+    origin: function (origin, callback) {
+        console.log("** Origin of request " + origin)
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            console.log("Origin acceptable")
+            callback(null, true)
+        } else {
+            console.log("Origin rejected")
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+app.use(cors(corsOptions))
+
+app.get('/api', (req, res) => {
+    res.send('hello world')
+})
+
 
 
 if (process.env.NODE_ENV === 'production') {
@@ -14,25 +34,6 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 }
-// ** MIDDLEWARE ** //
-// const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://salamet.herokuapp.com/']
-// const corsOptions = {
-//     origin: function (origin, callback) {
-//         console.log("** Origin of request " + origin)
-//         if (whitelist.indexOf(origin) !== -1 || !origin) {
-//             console.log("Origin acceptable")
-//             callback(null, true)
-//         } else {
-//             console.log("Origin rejected")
-//             callback(new Error('Not allowed by CORS'))
-//         }
-//     }
-// }
-// app.use(cors(corsOptions))
 
-
-app.get('/api', (req, res) => {
-    res.send('hello world')
-})
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log('Server Started on port ' + port))
