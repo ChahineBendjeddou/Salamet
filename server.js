b3afc70df020e384f5fb78d70b202bf362a788b9
ChahineBendjeddou <chahine.bendjeddou99@gmail.com>
2022-04-01 23:58:55 +0100

    testing

--- server.js
+++ server.js
@@ -15,20 +15,20 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 // ** MIDDLEWARE ** //
-const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://salamet.herokuapp.com/']
-const corsOptions = {
-    origin: function (origin, callback) {
-        console.log("** Origin of request " + origin)
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            console.log("Origin acceptable")
-            callback(null, true)
-        } else {
-            console.log("Origin rejected")
-            callback(new Error('Not allowed by CORS'))
-        }
-    }
-}
-app.use(cors(corsOptions))
+// const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://salamet.herokuapp.com/']
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         console.log("** Origin of request " + origin)
+//         if (whitelist.indexOf(origin) !== -1 || !origin) {
+//             console.log("Origin acceptable")
+//             callback(null, true)
+//         } else {
+//             console.log("Origin rejected")
+//             callback(new Error('Not allowed by CORS'))
+//         }
+//     }
+// }
+// app.use(cors(corsOptions))
 
 
 app.get('/api', (req, res) => {
