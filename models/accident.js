43dccdb5862079faa2558ee76696f2eccfebbf41
ChahineBendjeddou <chahine.bendjeddou99@gmail.com>
2022-04-03 00:45:02 +0100

    Creating the User and Accident Shemas

---
+++ models/accident.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+const accidentShema = new Schema({
+
+    location: {
+        type: [Number],
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    },
+    reporter: {
+        type: Schema.Type.ObjectId,
+        ref: 'User'
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    images: [ImageSchema]
+
+})
+
+
+module.exports = mongoose.model('Accident', accidentShema);
---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phone: {
+        type: Number,
+        unique: true
+    },
+    bloodType: {
+        type: String,
+        required: true
+    }
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', UserSchema);
