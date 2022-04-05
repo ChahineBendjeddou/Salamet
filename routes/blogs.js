49fd95d8754ff4745b2dbbf39abbbedbffa54d05
ChahineBendjeddou <chahine.bendjeddou99@gmail.com>
2022-04-06 00:28:48 +0100

    Adding the blogs routes

---
+++ routes/blogs.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
+const blogs = require('../controllers/blogs');
+
+
+
+module.exports = router;
