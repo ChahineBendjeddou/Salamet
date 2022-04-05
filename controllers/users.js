58833c9d69f5e95c57aa2b41d84cb5a68368c86b
ChahineBendjeddou <chahine.bendjeddou99@gmail.com>
2022-04-06 00:31:00 +0100

    Adding the users controllers

---
+++ controllers/users.js
@@ -0,0 +1,41 @@
+const User = require('../models/user');
+
+
+module.exports.renderRegister = (req, res) => {
+    res.render('users/register');
+}
+
+module.exports.register = async (req, res, next) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            const redirectUrl = req.session.returnTo || '/campgrounds';
+            delete req.session.returnTo;
+            res.redirect(redirectUrl);
+        })
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+}
+
+module.exports.renderLogin = (req, res) => {
+    res.render('users/login');
+}
+
+module.exports.login = (req, res) => {
+    req.flash('success', 'welcome back!');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+}
+
+module.exports.logout = (req, res) => {
+    req.logout();
+    // req.session.destroy();
+    req.flash('success', "Goodbye!");
+    res.redirect('/campgrounds');
+}
