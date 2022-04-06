c53b8c3aed98a4a6772d781c6da5ef0d7fde060b
ChahineBendjeddou <chahine.bendjeddou99@gmail.com>
2022-04-06 21:49:59 +0100

    making the app serve the react page and handle its routes

--- server.js
+++ server.js
@@ -31,14 +31,16 @@ app.get('/api', (req, res) => {
 
 
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    // Handle React routing, return all requests to React app
-    app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-}
+
+// Serve any static files
+app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+
+
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log('Server Started on port ' + port))
