6176e8ee239a3a8a377fc968042aebafcd8fa03b
ChahineBendjeddou <chahine.bendjeddou99@gmail.com>
2022-05-14 18:51:24 +0100

    changing stuff for heroku

--- database.js
+++ database.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/salamet'
+// const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/salamet'
+const dbUrl = 'mongodb+srv://chahine:chahine301299@cluster0.a969s.mongodb.net/salamet?retryWrites=true&w=majority'
 
 console.log(dbUrl)
 class Database {
