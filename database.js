5e9d157e2568c4521471ea4fbb20d467fdeecb9b
ChahineBendjeddou <chahine.bendjeddou99@gmail.com>
2022-05-18 06:41:54 +0100

    removing mongo atlas url

--- database.js
+++ database.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose')
 if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
-// const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/salamet'
-const dbUrl = 'mongodb+srv://chahine:chahine301299@cluster0.a969s.mongodb.net/salamet?retryWrites=true&w=majority'
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/salamet'
 
 console.log(dbUrl)
 class Database {
