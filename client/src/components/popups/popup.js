12a21165e335daa33f47e1ac4a3ad49544363bdb
ChahineBendjeddou <chahine.bendjeddou99@gmail.com>
2022-06-11 15:07:01 +0100

    Fixing the src of the img in the report pop-up

--- client/src/components/popups/popup.js
+++ client/src/components/popups/popup.js
@@ -2,37 +2,38 @@ import React from 'react'
 import './popup.css'
 import "../../routes/AddPost/add.css"
 import { MdAddCircleOutline } from "react-icons/md";
+import ambulance from '../../assets/ambulance.jpg'
 
 function popup(props) {
-  return(props.trigger) ? (
-        <div className='popup'>
-        <div className='popup-inner'>
-    <button className='close-btn' onClick={() => props.setTrigger(false)}> X </button>
-    {props.children}
-    
-    <div className="form">
-      <form>
-        <h1>REPORT AN ACCIDENT</h1>
-        <label>
-          <img src="/src/assets/ambulance.jpg" 
-          alt=""
-          className="addImg" />
-        </label>
+  return (props.trigger) ? (
+    <div className='popup'>
+      <div className='popup-inner'>
+        <button className='close-btn' onClick={() => props.setTrigger(false)}> X </button>
+        {props.children}
 
-        <label htmlFor="fileInput" className="add">
-            <MdAddCircleOutline className="addIcon"/> ADD PICTURE
-        </label>
+        <div className="form">
+          <form>
+            <h1>REPORT AN ACCIDENT</h1>
+            <label>
+              <img src={ambulance}
+                alt=""
+                className="addImg" />
+            </label>
 
-          <input id="fileInput" type="file" style={{ display: "none" }} />
-        <label>Description</label>
-        <textarea placeholder="Add a description"></textarea>
-    
-        <button className="button">SEND</button>
-      </form>
+            <label htmlFor="fileInput" className="add">
+              <MdAddCircleOutline className="addIcon" /> ADD PICTURE
+            </label>
+
+            <input id="fileInput" type="file" style={{ display: "none" }} />
+            <label>Description</label>
+            <textarea placeholder="Add a description"></textarea>
+
+            <button className="button">SEND</button>
+          </form>
+        </div>
+      </div>
     </div>
-</div>
-</div>
-  ) :"";
+  ) : "";
 }
 
 export default popup
