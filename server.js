71ebfc5c49ed39bc37c725cb061f6d95d975b722
ChahineBendjeddou <chahine.bendjeddou99@gmail.com>
2022-06-14 22:36:08 +0100

    change session resave to true

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const sessionConfig = {
     store,
     name: 'session',
     secret,
-    resave: false,//don't save session if unmodified
+    resave: true,//don't save session if unmodified
     saveUninitialized: true,// don't create session until something stored
     cookie: {
         httpOnly: true,
