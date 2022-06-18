dc00c571bcd2a5a802ebb96de942d8ab160eaa14
ChahineBendjeddou <chahine.bendjeddou99@gmail.com>
2022-06-18 10:28:50 +0100

    testing cors

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,21 +3,11 @@ const deepSanitize = require('../utils/deepSanitize')
 const sanitize = require('mongo-sanitize')
 module.exports.addBlog = async (req, res) => {
     try {
-        // const newBlog = new Blog(req.body.blog)
-        // newBlog.images = req.files.map(img => ({ url: img.path, filename: img.filename }))
-        // newBlog.author = req.user._id
-        // await newBlog.save()
-        // console.log(await newBlog.populate('author'))
-
-        const { title, body, type } = req.body.blog
-        console.log(deepSanitize(body))
-        console.log(deepSanitize(title))
-        console.log(sanitize(title))
-        console.log(deepSanitize(type))
-        console.log(title === { _id: { '$ne': 1 } })
-        console.log()
-        // const newBody = req.body.blog.map(e => deepSanitize(e))
-        // console.log(newBody)
+        const newBlog = new Blog(req.body.blog)
+        newBlog.images = req.files.map(img => ({ url: img.path, filename: img.filename }))
+        newBlog.author = req.user._id
+        await newBlog.save()
+        console.log(await newBlog.populate('author'))
         res.redirect('/BlogHome')
     } catch (err) { console.log(err) }
 }
