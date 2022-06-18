b7c5bc799cc1c3f61c1a894ece02d5ac0beb4c6c
ChahineBendjeddou <chahine.bendjeddou99@gmail.com>
2022-06-18 09:30:57 +0100

    testing cors

--- middleware.js
+++ middleware.js
@@ -1,11 +1,12 @@
-const whitelist = ['http://localhost:3000/*', 'http://localhost:5000/*', 'https://salamet.herokuapp.com/*', 'https://cloudinary.com/*', 'https://cloud.mongodb.com/*']
+const whitelist = ['http://localhost:3000/*', 'http://localhost:5000/*', 'https://salamet.herokuapp.com', 'https://cloudinary.com/*', 'https://cloud.mongodb.com/*']
 module.exports.corsOptions = {
     origin: function (origin, callback) {
-        console.log("** Origin of request " + origin)
         if (whitelist.indexOf(origin) !== -1 || !origin) {
+            console.log("** Origin of request accepted " + origin)
             console.log("Origin acceptable")
             callback(null, true)
         } else {
+            console.log("** Origin of request not accepted" + origin)
             console.log("Origin rejected")
             callback(new Error('Not allowed by CORS'))
         }
